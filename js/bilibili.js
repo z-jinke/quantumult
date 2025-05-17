cb24fc9a5ac9ce8f8e973521a3cce93c799677a7
Z <155460219+z-jinke@users.noreply.github.com>
2025-05-17 16:50:11 +0800

    更新bilibili.js

--- js/bilibili.js
+++ js/bilibili.js
@@ -59,12 +59,10 @@ if (/^https:\/\/app\.bilibili\.com\/x\/v2\/account\/mine/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
         if (url.includes("/ipad")) {
-            obj.ttl = 18000;
             delete obj.data.ipad_upper_sections;
             obj.data.ipad_recommend_sections = [{"id":789,"title":"关注列表","uri":"bilibili://user_center/myfollows","icon":"http://i0.hdslb.com/bfs/feed-admin/fdd7f676030c6996d36763a078442a210fc5a8c0.png","mng_resource":{}},{"id":790,"title":"消息列表","uri":"bilibili://link/im_home","icon":"http://i0.hdslb.com/bfs/feed-admin/e1471740130a08a48b02a4ab29ed9d5f2281e3bf.png","mng_resource":{}}];
             obj.data.ipad_more_sections = [{"id":797,"title":"官方客服","uri":"bilibili://user_center/feedback","icon":"http://i0.hdslb.com/bfs/feed-admin/7801a6180fb67cf5f8ee05a66a4668e49fb38788.png","mng_resource":{}},{"id":798,"title":"设置中心","uri":"bilibili://user_center/setting","icon":"http://i0.hdslb.com/bfs/feed-admin/34e8faea00b3dd78977266b58d77398b0ac9410b.png","mng_resource":{}}];
         } else {
-            obj.ttl = 18000;
             obj.data.use_modular_vip_section = false;
             obj.data.sections_v2 = [{"items":[{"id":396,"title":"离线缓存","icon":"http://i0.hdslb.com/bfs/archive/5fc84565ab73e716d20cd2f65e0e1de9495d56f8.png","common_op_item":{},"uri":"bilibili://user_center/download"},{"id":397,"title":"历史记录","icon":"http://i0.hdslb.com/bfs/archive/8385323c6acde52e9cd52514ae13c8b9481c1a16.png","common_op_item":{},"uri":"bilibili://user_center/history"},{"id":3072,"title":"我的收藏","icon":"http://i0.hdslb.com/bfs/archive/d79b19d983067a1b91614e830a7100c05204a821.png","common_op_item":{},"uri":"bilibili://user_center/favourite?version=2"},{"id":2830,"title":"稍后再看","icon":"http://i0.hdslb.com/bfs/archive/63bb768caa02a68cb566a838f6f2415f0d1d02d6.png","need_login":1,"uri":"bilibili://user_center/watch_later_v2","common_op_item":{}}],"style":1,"button":{}},{"items":[{"id":407,"title":"客服中心","icon":"http://i0.hdslb.com/bfs/feed-admin/7801a6180fb67cf5f8ee05a66a4668e49fb38788.png","common_op_item":{},"uri":"bilibili://user_center/feedback"},{"id":410,"title":"我的设置","icon":"https://i0.hdslb.com/bfs/feed-admin/34e8faea00b3dd78977266b58d77398b0ac9410b.png","common_op_item":{},"uri":"bilibili://user_center/setting"}],"style":2,"button":{}}];
         }
