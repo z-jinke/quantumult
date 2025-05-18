df3afac0fc381a29d180404784cffb6301b5cfdd
Z <155460219+z-jinke@users.noreply.github.com>
2025-05-18 21:35:16 +0800

    更新networkip.js

--- js/networkip.js
+++ js/networkip.js
@@ -2,7 +2,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 let title = " ";
-let subtitle = obj.ip;
+let subtitle = obj.ip + " - " + obj.country_iso;
 let ip = obj.ip;
 
 $done({ title, subtitle, ip });
